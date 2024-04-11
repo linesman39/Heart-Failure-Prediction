a3ae6ce80d0185fbc0fb30a4e730a69809ce6722
Eyetonghan Denyefa Callistus <80490194+linesman39@users.noreply.github.com>
2024-04-11 22:00:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,22 +25,6 @@ def run():
 
     model = pd.read_pickle("xgboost_heart_disease_detection_v1.pkl")
 
-def add_bg_from_url():
-st.markdown(
-        f"""
-        <style>
-        .stApp {{
-            background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
-            background-attachment: fixed;
-            background-size: cover
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-add_bg_from_url()
-
     st.markdown(
         """
 <style>
