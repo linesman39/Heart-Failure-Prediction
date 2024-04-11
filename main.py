692718332797f12dc4d7fb63eeca65bcc0e1d86f
Eyetonghan Denyefa Callistus <80490194+linesman39@users.noreply.github.com>
2024-04-11 14:52:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def run():
         unsafe_allow_html=True
     )
 
-    header = st.container(height = 300)
+    header = st.container(height = 200)
     content = st.container(height=1500)
 
     st.write("")
@@ -132,7 +132,7 @@ def run():
         st.write("")
 
     with content:
-        col1, col2 = st.columns([20, 9])
+        col1, col2 = st.columns([9, 20])
 
         with col1:
             with st.form("Predict"):
