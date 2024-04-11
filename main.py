9f26d8196ada7f75a32337ca79788aa9ba725b04
Eyetonghan Denyefa Callistus <80490194+linesman39@users.noreply.github.com>
2024-04-11 16:57:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -124,6 +124,7 @@ def run():
 
     header = st.container()
     content = st.container()
+    page2 = st.container()
 
     st.write("")
 
@@ -176,7 +177,12 @@ def run():
 
                 predict_button = st.form_submit_button("Predict 🚀")
 
-        with col2:
+
+
+        
+    with content:
+        col3 = st.columns([5])
+        with col3:
             if predict_button:
                 patient_fasting_blood_sugar = 1
                 if fasting_blood_sugar == "Less Than 120 mg/dl":
@@ -231,44 +237,35 @@ def run():
                 new_data.extend(patinet_ecg)
                 new_data.extend(patient_exercise_angina)
                 new_data.extend(patient_slope)
+    
+                with st.spinner(text='Predict The Value..'):
 
-    result_container = st.container()
-    with result_container:
-        # Appending All Data
-        new_data.extend(patient_gender)
-        new_data.extend(patient_chest_pain_type)
-        new_data.extend(patinet_ecg)
-        new_data.extend(patient_exercise_angina)
-        new_data.extend(patient_slope)
-        
-        with st.spinner(text='Predict The Value..'):
+                    predicted_value = model.predict([new_data])[0]
+                    prediction_prop = np.round(
+                    model.predict_proba([new_data])*100)
+                    sleep(1.2)
 
-            predicted_value = model.predict([new_data])[0]
-            prediction_prop = np.round(
-            model.predict_proba([new_data])*100)
-            sleep(1.2)
+                    heart_disease, no_heart_disease = st.columns(2)
 
-            heart_disease, no_heart_disease = st.columns(2)
+                    st.image("imgs/heartbeat.png",
+                            caption="", width=100)
+                    if predicted_value == 0:
+                        st.subheader("Expected He Is")
+                        st.subheader(":green[Not a Heart Patient]")
 
-            st.image("imgs/heartbeat.png",
-                    caption="", width=100)
-            if predicted_value == 0:
-                st.subheader("Expected He Is")
-                st.subheader(":green[Not a Heart Patient]")
+                    else:
+                        st.subheader(f"Expected He Is")
+                        st.subheader(":red[Heart Patient]")
 
-            else:
-                st.subheader(f"Expected He Is")
-                st.subheader(":red[Heart Patient]")
+                    with heart_disease:
+                        st.image("imgs/heart.png", caption="", width=65)
+                        st.subheader(":green[*Not Heart Patient*]")
+                        st.subheader(f"{prediction_prop[0, 0]}%")
 
-            with heart_disease:
-                st.image("imgs/heart.png", caption="", width=65)
-                st.subheader(":green[*Not Heart Patient*]")
-                st.subheader(f"{prediction_prop[0, 0]}%")
-
-            with no_heart_disease:
-                st.image("imgs/hearted.png", caption="", width=65)
-                st.subheader(f":red[*Heart Patient*]")
-                st.subheader(f"{prediction_prop[0, 1]}%")
+                    with no_heart_disease:
+                        st.image("imgs/hearted.png", caption="", width=65)
+                        st.subheader(f":red[*Heart Patient*]")
+                        st.subheader(f"{prediction_prop[0, 1]}%")
 
 
 run()
